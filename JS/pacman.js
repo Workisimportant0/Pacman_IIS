61d2a564ca02dc55f9b229e28ab446bf890fa5a4
Workisimportant0 <islam.ismailov2154@gmail.com>
2021-09-07 15:57:10 +0200

    nouvelle version

--- JS/pacman.js
+++ JS/pacman.js
@@ -1,4 +1,4 @@
-
+let score = 0
 let grille = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 2, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 0],
@@ -10,7 +10,7 @@ let grille = [
     [0, 0, 0, 0, 2, 0, 0, 0, 2, 0, 2, 0, 0, 0, 2, 0, 0, 0, 0],
     [0, 1, 1, 0, 2, 0, 2, 2, 2, 2, 2, 2, 2, 0, 2, 0, 1, 1, 0],
     [0, 0, 0, 0, 2, 0, 2, 0, 0, 2, 0, 0, 2, 0, 2, 0, 0, 0, 0],
-    [2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2],
+    [0, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 0],
     [0, 0, 0, 0, 2, 0, 2, 0, 0, 2, 0, 0, 2, 0, 2, 0, 0, 0, 0],
     [0, 1, 1, 0, 2, 0, 2, 2, 2, 2, 2, 2, 2, 0, 2, 0, 1, 1, 0],
     [0, 0, 0, 0, 2, 0, 2, 0, 0, 0, 0, 0, 2, 0, 2, 0, 0, 0, 0],
@@ -22,14 +22,49 @@ let grille = [
     [0, 2, 0, 0, 0, 0, 0, 0, 2, 0, 2, 0, 0, 0, 0, 0, 0, 2, 0],
     [0, 2, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-
 ];
 
-//500ms
+let fontomeRouge = {
+    x: 9,
+    y: 11,
+    direction: 0
+}
+let fontomVert = {
+    x: 7,
+    y: 11,
+    direction: 0
+}
+let fontomeOrange = {
+    x: 7,
+    y: 12,
+    direction: 0
+}
 
-/*function affichePlateau() {
+let pacman = {
+    x: 1,
+    y: 2,
+    direction: 0
+}
+function tourDeJeu() {
+    affichePlateau()
+    deplacerPackman()
+    collisionPackman()
+    affichePackman()
+    collisionPackman()
+    mangerBonbon()
+    afficheFontome1()
+    afficheFontome2()
+    afficheFontome3()
+    afficherScore()
+}
+
+let plateau = document.querySelector('.container')
+
+function affichePlateau() {
+    plateau.innerHTML = ""
     for (let i in grille) {
         for (let j in grille[i]) {
+
             let monElement = document.createElement("div");
             if (grille[i][j] == 0) {
                 monElement.className = "mur";
@@ -41,47 +76,114 @@ let grille = [
                 monElement.className = "bonbon";
             }
 
-
-           affichePlateau.appendChild(monElement);
+            monElement.style.gridArea = (+i + 1) + " / " + (+j + 1)
+            plateau.appendChild(monElement);
         }
-    }
-}*/
-
-
-let affichePlateau = document.querySelector('.container')
-for (let i in grille) {
-    for (let j in grille[i]) {
-        let monElement = document.createElement("div");
-        if (grille[i][j] == 0) {
-            monElement.className = "mur";
-        }
-        if (grille[i][j] == 1) {
-            monElement.className = "sol";
-        }
-        if (grille[i][j] == 2) {
-            monElement.className = "bonbon";
-        }
-
-
-        affichePlateau.appendChild(monElement);
     }
 }
 
-let packman = [[3]]
-let affichePackman = document.querySelector('.container')
-for (let u in packman) {
-    for (let o in packman[u]) {
-        let monPacman = document.createElement("div")
-        if (packman[u][o] == 3) {
-            monPacman.className = "packman"
+setInterval(tourDeJeu, 500)
+
+document.body.addEventListener("keyup", touches)
+function touches(event) {
+    console.log(event)
+    if (event.key == "ArrowRight") {
+        pacman.direction = 0
+    }
+    if (event.key == "ArrowDown") {
+        pacman.direction = 1
+    }
+    if (event.key == "ArrowLeft") {
+        pacman.direction = 2
+    }
+    if (event.key == "ArrowUp") {
+        pacman.direction = 3
+    }
+}
+
+function affichePackman() {
+    let monPacman = document.createElement("div")
+    monPacman.className = "pacman"
+
+    monPacman.style.gridArea = pacman.y + " / " + pacman.x
+    plateau.appendChild(monPacman);
+}
+
+function afficheFontome1() {
+    let fontome1 = document.createElement("div")
+    fontome1.className = "fontomeRouge"
+
+    fontome1.style.gridArea = fontomeRouge.y + " / " + fontomeRouge.x
+    plateau.appendChild(fontome1);
+}
+
+function afficheFontome2() {
+    let fontome2 = document.createElement("div")
+    fontome2.className = "fontomeVert"
+
+    fontome2.style.gridArea = fontomVert.y + " / " + fontomVert.x
+    plateau.appendChild(fontome2);
+}
+
+function afficheFontome3() {
+    let fontome3 = document.createElement("div")
+    fontome3.className = "fontomeOrange"
+
+    fontome3.style.gridArea = fontomeOrange.y + " / " + fontomeOrange.x
+    plateau.appendChild(fontome3);
+}
+
+function deplacerPackman() {
+    if (pacman.direction == 0) {
+        pacman.x++
+    }
+    if (pacman.direction == 1) {
+        pacman.y++
+    }
+    if (pacman.direction == 2) {
+        pacman.x--
+    }
+    if (pacman.direction == 3) {
+        pacman.y--
+    }
+}
+
+function collisionPackman() {
+    if (grille[pacman.y - 1][pacman.x - 1] == 0) {
+        if (pacman.direction == 0) {
+            pacman.x--
         }
-        affichePackman.appendChild(monPacman)
+        if (pacman.direction == 1) {
+            pacman.y--
+        }
+        if (pacman.direction == 2) {
+            pacman.x++
+        }
+        if (pacman.direction == 3) {
+            pacman.y++
+        }
+    }
+}
+
+function mangerBonbon() {
+    if (grille[pacman.y - 1][pacman.x - 1] == 2) {
+        grille[pacman.y - 1][pacman.x - 1] = 1
+        score+=10
+    }
+}
+
+function afficherScore(){
+    let divScore = document.querySelector('.score');
+    divScore.innerHTML="score"+score;
+    if (score>=100){
+        alert("Vous avez terminé le jeu")
+    }else if(score>=100){
+        clearInterval
     }
 
 }
-function tourDeJeu() {
-    setInterval(function () { alert("Attaque"); }, 500);
-}
+
+//score pour gagné est de 1930
 
 
 
